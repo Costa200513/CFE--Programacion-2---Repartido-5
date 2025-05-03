631e3c3694280aecd872e724d858420d069e6449
Costa200513 <costaezequiel56@gmail.com>
2025-05-03 20:51:09 -0300

    Subiendo Repartido 5

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -2,7 +2,7 @@ public class Ejercicio8
 {
     public static void main(String[] args)
     {
-        System.out.println(validarPassword("holaajsdgasu"));
+        System.out.println(validarPassword(""));
     }
     public static boolean validarPassword(String clave)
     {
